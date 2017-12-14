4e587f2174e179933779d8e4707c4e743cdfe4eb
brian greenfield <briang@cpan.org>
2017-12-14 14:33:09 +0000

    add release script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME="blueprint_flipper"
+FILES="graphics locale prototypes control.lua data.lua info.json"
+
+DEST="${NAME}_$V"
+ZIP="${DEST}.zip"
+
+if [[ "$V" == "" ]] ; then
+    echo "error: \$V not set"
+else
+    [[ -e "$ZIP" ]] && rm "$ZIP"
+    [[ -f "$DEST" ]] || mkdir "$DEST"
+    rm -rf "$DEST/*"
+    for f in $FILES ; do cp -a "$f" "$DEST" ; done
+    zip -r "$ZIP" "$DEST"
+fi
